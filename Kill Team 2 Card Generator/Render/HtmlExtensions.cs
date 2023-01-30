fdd9b1f26432514c0aaa046d01260a98adb51095
Rune Højsgaard <rho@monthio.com>
2023-01-30 15:25:33 +0100

    Added resources and changed cards sized to American Standard. Added printing of weapons

---
+++ Kill Team 2 Card Generator/Render/HtmlExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace KT2CG.Render
+{
+	public static class HtmlExtensions
+	{
+		public static string ReplaceRangesWithSpans(this string text)
+		{
+			return text.Replace("△", "<span class='range1' ></span>")
+				.Replace("◯", "<span class='range2' ></span>")
+				.Replace("□", "<span class='range3' ></span>")
+				.Replace("⬠", "<span class='range6' ></span>");
+		}
+
+		public static string ReplaceLineBreaksWithBreaks(this string text)
+		{
+			return text.Replace(Environment.NewLine, "<br />");
+		}
+
+		public static string BoldFromLineBreakToColon(this string text)
+		{
+			return Regex.Replace(text, @"<br />(.*?)\:", "<br /><b>$1</b>:<br />");
+		
+		}
+	}
+}
--- Kill Team 2 Card Generator/Render/HtmlPrinter.cs
+++ Kill Team 2 Card Generator/Render/HtmlPrinter.cs
@@ -1,5 +1,7 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using System.Text.Json;
+using KT2CG.Properties;
 using KT2CG.Scraping;
 
 namespace KT2CG.Render;
@@ -37,51 +39,149 @@ public class HtmlPrinter
 		// do the HTML magic
 
 		stringBuilder.Append(@$"
-		<!DOCTYPE HTML PUBLIC ""-//W3C//DTD HTML 4.01//EN""
-		  ""http://www.w3.org/TR/html4/strict.dtd"">
-		<HTML>
-		  <HEAD>
-		  <STYLE>
-			  {RenderStyles()}
-		  </STYLE>
-		  </HEAD>
-		  <BODY>
-			{RenderPages()}
-		  </BODY>
-		</HTML>");
+<!DOCTYPE HTML PUBLIC ""-//W3C//DTD HTML 4.01//EN""
+  ""http://www.w3.org/TR/html4/strict.dtd"">
+<HTML>
+  <HEAD>
+  <STYLE>
+	  {RenderStyles()}
+  </STYLE>
+  </HEAD>
+  <BODY>
+	{RenderPages()}
+  </BODY>
+</HTML>");
 
 
 		Directory.CreateDirectory(_outputFolder);
 		File.WriteAllText(_outputFolder + "\\teams.html", stringBuilder.ToString());
+		File.WriteAllBytes(_outputFolder + "\\r1.png", Resources.f1);
+		File.WriteAllBytes(_outputFolder + "\\r2.png", Resources.f2);
+		File.WriteAllBytes(_outputFolder + "\\r3.png", Resources.f3);
+		File.WriteAllBytes(_outputFolder + "\\r6.png", Resources.f6);
+	}
+
+	private List<Equipment> ExpandEqipment(List<Equipment> equipments)
+	{
+		List<Equipment> expandedEquipments = new List<Equipment>();
+		const int equipmentPoints = 10;
+
+		foreach (var equipment in equipments)
+		{
+			if (equipment.Name.EndsWith("\u002B"))
+			{
+				expandedEquipments.Add(equipment);
+			}
+			else if (int.TryParse(equipment.Cost, out var equipmentCost))
+			{
+				int copies = equipmentPoints / equipmentCost;
+				for (int i = 0; i < copies; i++)
+				{
+					expandedEquipments.Add(equipment);
+				}
+			}
+			else
+			{
+				int copies = 4;
+				for (int i = 0; i < copies; i++)
+				{
+					expandedEquipments.Add(equipment);
+				}
+			}
+		}
+
+		return expandedEquipments;
+	}
+
+
+	private void RenderEqipmentPages(KillTeam killTeam, ref StringBuilder sb)
+	{
+		int itemsPerPage = 9;
+		List<Equipment> items = ExpandEqipment(killTeam.Equipment);
+		int pages = (items.Count + itemsPerPage - 1) / itemsPerPage;
+
+		for (int i = 0; i < pages; i++)
+		{
+			int firstItem = i * itemsPerPage;
+			var lastItem = firstItem + itemsPerPage;
+			if (lastItem > items.Count)
+				lastItem = items.Count;
+
+			sb.Append(@"
+		<div class='page'>
+		   <div class='grid'>");
+
+			for (var j = firstItem; j < lastItem; j++)
+			{
+				sb.Append(@$" 
+			<div class='equipment'>
+			  <div class='equipmentInside'>
+				<div class='equipment-header'>
+				 <div class='name'>{items[j].Name}</div>
+				  <div class='EP'>[{items[j].Cost}]</div>
+				</div>
+				<div class='description'>{FormatDescription(items[j].Description) + FormatWeapons(items[j].Weapons)} </div>
+			  </div>
+			</div>");
+			}
+
+			sb.Append(@"
+		  </div>
+		</div>");
+		}
+	}
+
+	private string FormatDescription(string description)
+	{
+		return description.ReplaceRangesWithSpans().ReplaceLineBreaksWithBreaks().BoldFromLineBreakToColon();
+	}
+
+	private string FormatWeapons(List<Weapon> weapons)
+	{
+		StringBuilder sb = new StringBuilder();
+		foreach (var weapon in weapons)
+		{
+			sb.AppendLine("<br/><br/>");
+			sb.AppendLine("<b>" + weapon.Name + "</b><br/>");
+			sb.AppendLine(weapon.IsRanged ? "Ranged weapon<br/>" : "Melee weapons<br/>");
+			sb.AppendLine("A:<span>&#160;&#160;&#160;&#160;</span>" + weapon.Attacks + "<br/>");
+			sb.AppendLine("S:<span>&#160;&#160;&#160;&#160;</span>" + weapon.Skill + "<br/>");
+			sb.AppendLine("D:<span>&#160;&#160;&#160;&#160;</span>" + weapon.Damage + "<br/>");
+
+			string critRules = "";
+			foreach (var rule in weapon.OnCrit)
+			{
+				critRules += rule + ", ";
+			}
+
+			critRules = critRules.Length > 2 ? critRules.Substring(0, critRules.Length - 2) : "-";
+
+			sb.AppendLine("C:<span>&#160;&#160;&#160;&#160;</span>" + critRules + "<br/>");
+
+
+			string specialRules = "";
+			foreach (var rule in weapon.SpecailRules)
+			{
+				specialRules += rule + ", ";
+			}
+
+			specialRules = specialRules.Length > 0 ? specialRules.Substring(0, specialRules.Length - 2) : "-";
+
+			sb.AppendLine("S:<span>&#160;&#160;&#160;&#160;</span>" + specialRules + "<br/>");
+		}
+
+		return sb.ToString().ReplaceRangesWithSpans();
 	}
 
 	private string RenderPages()
 	{
-		// foreach faction
-		// Render equipment in american mini size in chunks of A4 (my guess is 4x4 on a page)
-		// - Only one of equipment ending in \u002B
-		// - 5 of other equipments OR as many as you can wholly buy for 10 EP OR based on input options. If multiple EP, just assume 4.
-		// Render Tac Ops, Strategic Ploys and Tactical Ploys in Tarot sized cards on A4. There should be 4 on a A4 page.
-		var testdata = @"<div class='page'>
-					  <div class='grid'>"
-			;
+		StringBuilder sb = new StringBuilder();
+		foreach (var killTeam in _killTeams)
+		{
+			RenderEqipmentPages(killTeam, ref sb);
+		}
 
-		for (var i = 0; i < 16; i++)
-			testdata += @" <div class='equipment'>
-					   <div class='equipmentInside'>
-					     <div class='equipment-header'>
-                            <div class='name'>Horse head</div>
-	          		        <div class='EP'>[1]</div>
-                        </div>
-					     <div class='description'> Foo bar bas </div>
-	          	       </div>
-				   </div>
-	          ";
-
-		testdata += "</div>" +
-		            "</div>";
-
-		return testdata;
+		return sb.ToString();
 	}
 
 
@@ -95,46 +195,80 @@ public class HtmlPrinter
 			 max-height: 29.7cm;
 			 display: block;
 			 margin: 0.5cm 0.5cm 0.5cm 0.5cm;
-			 box-shadow: 0 0 0.5cm rgba(0, 0, 0, 0.5);
+			 /* box-shadow: 0 0 0.5cm rgba(0, 0, 0, 0.5); */
 			 padding: 1cm 1cm 1cm 1cm;
 		}
 		 .grid {
 			 display: grid;
-			 grid-template-columns: repeat(auto-fit, 44mm);
-			 grid-template-rows: repeat(auto-fit, 63mm);
+			 grid-template-columns: repeat(auto-fit, 57mm);
+			 grid-template-rows: repeat(auto-fit, 89mm);
 			 gap: 0 0;
 		}
 		 .equipment {
 			 padding: 2px;
 			 color: #c54c21;
-			 height: 63mm;
-			 width: 44mm;
+			 height: 89mm;
+			 width: 57mm;
 			 background: linear-gradient(-45deg, transparent 14px, #c54c21 0) 
 		}
 		 .equipmentInside {
 			 display: block;
 			 padding: 0px;
-			 height: 63mm;
+			 height: 89mm;
 			 background: linear-gradient(-45deg, transparent 13px, #FFFFFF 0);
 		}
          .equipment-header {
 		     display: flex;
 		     justify-content: space-between;
-            background: #c54c21;
-            align-items: center;
-            padding: 5px 5px;
-		}
+			 background: #c54c21;
+			 align-items: center;
+			 padding: 5px 5px;
+			 height: 40px;
+			 font-size: 100%;
+			 font-weight: bold;
+		 }	 
 		 .name, .EP {
-			 font-size: 120%;
+			 font-size: 100%;
 			 color: white;
-			 
+			 font-weight: normal;
 		}
 		 .description{
 			 color: black;
-            padding: 5px 5px;
+			 padding: 5px 5px;
 			 background:linear-gradient(315deg, transparent 13px, #FFFFFF 0) 
 		}
 
-			  ";
+		.range1 {
+			 display: inline-block;
+			 height: 19px;
+			 width: 22px;
+			 vertical-align: middle;
+			 background-image: url(r1.png);
+		}
+
+		.range2 {
+			 display: inline-block;
+			 height: 19px;
+			 width: 19px;
+			 vertical-align: middle;
+			 background-image: url(r2.png);
+		}
+
+		.range3 {
+			 display: inline-block;
+			 height: 19px;
+			 width: 19px;
+			 vertical-align: middle;
+			 background-image: url(r3.png);
+		}
+
+		.range6 {
+			 display: inline-block;
+			 height: 19px;
+			 width: 22px;
+			 vertical-align: middle;
+			 background-image: url(r6.png);
+		}
+		 ";
 	}
 }
